ea0df0f20eb651559d19132063b99c6835e851eb
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-02-26 16:58:05 +0100

    playing around with tests

--- tests/utils/test_astutils.py
+++ tests/utils/test_astutils.py
@@ -1,5 +1,12 @@
+import astutils
+import ast
 import pytest
 
+simple_assign = "x = 3"
+simple_aug_assign = "x, y = 3"
+simple_ann_assign = "x: int = 3"
 
-def test_var_contains_assign():
-    assert True == True
+
+def test_var_contains_assign_positive():
+    print("HERE")
+    assert astutils.var_contains_assign("y", ast.parse(simple_assign))
