b4d937220385145c2129d0dfc8f27ed113c585e4
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-03-09 12:30:47 +0100

    progress

--- src/dataflow/dataflow.py
+++ src/dataflow/dataflow.py
@@ -1,29 +1,89 @@
 from typing import List
 
 
-class Operator:
-    pass
-
-
 class Event:
     def __init__(
         self,
         event_name: str,
-        event_state: dict,
+        argument_state: dict,
         operator_state: dict,
         hidden_state: dict,
     ):
         self.event_name = event_name
-        self.event_state = event_state
+        self.argument_state = argument_state
         self.operator_state = operator_state
         self.hidden_state = hidden_state
 
 
+class Operator:
+    def __init__(
+        self,
+        name: str,
+        incoming_edges: List["Edge"],
+        outgoing_edges: List["Edge"],
+        input_event_name: str,
+        output_event_name: str,
+    ):
+        self.name = name
+        self.incoming_edges = incoming_edges
+        self.outgoing_edges = outgoing_edges
+
+        self.input_event_name = input_event_name
+        self.output_event_name = output_event_name
+
+        for edge in incoming_edges:
+            if edge.get_event().event_name != self.input_event_name:
+                raise AttributeError(
+                    f"Operator {name} only accepts incoming edges with events of type {input_event_name}, but got an edge of type {edge.get_event().event_name}."
+                )
+
+        for edge in outgoing_edges:
+            if edge.get_event().event_name != self.input_event_name:
+                raise AttributeError(
+                    f"Operator {name} only accepts outgoing edges with events of type {output_event_name}, but got an edge of type {edge.get_event().event_name}."
+                )
+
+    def init_operator(self):
+        pass
+
+    def invoke(self, event: Event) -> Event:
+        if self.input_event_name != event.event_name:
+            raise AttributeError(
+                f"Expected an {self.input_event_name} event to invoke {self.name}, but got an {event.event_name} event."
+            )
+
+        print(f"Invoked {self.name}.")
+        # Actually call the function here.
+
+        output_event = Event(self.output_event_name, None, None, None)
+        return output_event
+
+
+class StateOperator(Operator):
+    def init_operator(self):
+        pass
+
+
 class Edge:
-    def __init__(self, from_operator: Operator, to_operator: Operator, event: Event):
+    def __init__(self, from_operator: Operator, to_operator: Operator, event_name: str):
         self.from_operator = from_operator
         self.to_operator = to_operator
-        self.event = event
+        self.event_name = event_name
+
+    def get_event(self) -> Event:
+        return self.event
+
+    def flow(self, event: Event) -> List[Event]:
+        if not self.to_operator:
+            return [self.event]
+
+        output_event: Event = self.to_operator.invoke(event)
+
+        outputs = []
+        for edge in self.to_operator.outgoing_edges:
+            outputs = outputs + edge.flow(output_event)
+
+        return outputs
 
 
 class Ingress:
@@ -46,3 +106,13 @@ class Dataflow:
         self.edges = edges
         self.ingresses = ingresses
         self.egresses = egresses
+
+    def get_edge_by_event(self, event: Event) -> Edge:
+        edge_list = [edge for edge in self.edges if edge.get_event() == event]
+
+        if len(edge_list) > 1:
+            raise AttributeError(
+                f"Event {event} is used on multiple edges. That should not be possible."
+            )
+
+        return edge_list[0]
---
+++ src/runtime/test_runtime.py
@@ -0,0 +1,11 @@
+from dataflow import *
+
+
+class TestRuntime:
+    def __init__(self, dataflow: Dataflow):
+        self.dataflow = Dataflow
+
+    def execute_flow(self, input_event: Event) -> List[Event]:
+        first_edge: Edge = self.dataflow.get_edge_by_event(input_event)
+
+        first_edge.flow()
