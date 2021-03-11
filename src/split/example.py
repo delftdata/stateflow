a5ddcb2effacf4873d0b188acf1adaa4346131c4
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-03-11 13:37:56 +0100

    Update example.py

--- src/split/example.py
+++ src/split/example.py
@@ -12,7 +12,20 @@ class Calculator:
 
     def computation(self, y):
         a = self.x + 1
-        sqr(y + 2)
-        sqr(y + 3)
-        c = a + self.x
+        b = sqr(y + 2) + sqr(y + 3)
+        c = a + self.x + b
+        return c
+
+    def fun_0(self, y):
+        a = self.x + 1
+        sqr_0_arg_0 = y + 2
+        return a, sqr_0_arg_0
+
+    def fun_1(self, y):
+        sqr_1_arg_0 = y + 3
+        return sqr_1_arg_0
+
+    def fun_2(self, a, sqr_0_result, sqr_1_result):
+        b = sqr_0_result + sqr_1_result
+        c = a + self.x + b
         return c
