ab071af498897f94518b951f5573cc07bf0d196e
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-03-17 17:37:41 +0100

    Update extract_stateful.py

--- src/analysis/extract_stateful.py
+++ src/analysis/extract_stateful.py
@@ -2,17 +2,19 @@ from typing import List, Tuple, Any, Optional
 import libcst as cst
 from dataflow.stateful_fun import StatefulFun
 import libcst.matchers as m
+import libcst.helpers as helpers
 
 
 class ExtractStatefulFun(cst.CSTVisitor):
-    def __init__(self):
+    def __init__(self, module_node: cst.CSTNode):
+        self.module_node = module_node
         self.is_defined: bool = False
         self.class_name: str = None
 
         self.self_attributes: List[Tuple[str, Any]] = []
 
     def extract_types(self, node: cst.Annotation) -> Any:
-        pass
+        return eval(self.module_node.code_for_node(node.annotation))
 
     def is_self(self, node: cst.CSTNode) -> bool:
         if m.matches(node, m.Attribute(value=m.Name(value="self"))):
@@ -22,27 +24,30 @@ class ExtractStatefulFun(cst.CSTVisitor):
 
     def visit_AnnAssign(self, node: cst.AnnAssign) -> Optional[bool]:
         if self.is_self(node.target) and m.matches(node.target.attr, m.Name()):
-            print(node.annotation.annotation)
+            annotation = self.extract_types(node.annotation)
             print(
-                f"Found class variable {node.target.attr.value} with type {node.annotation.annotation.value}"
+                f"Found class variable {node.target.attr.value} with type {annotation}."
             )
 
     def visit_AugAssign(self, node: cst.AugAssign) -> Optional[bool]:
-        if self.is_self(node.target):
-            print(f"Found class variable {node.attr}.")
+        if self.is_self(node.target) and m.matches(node.target.attr, m.Name()):
+            print(f"Found class variable {node.target.attr.value} without a type hint.")
 
-    def visit_Assign(self, node: cst.Assign) -> Optional[bool]:
-        for target in node.targets:
-            print("HERE")
-            if self.is_self(target):
-                print(f"Jeej {target}")
+    def visit_AssignTarget(self, node: cst.AssignTarget) -> None:
+        if not m.matches(node, m.AssignTarget(target=m.Tuple())):
+            if self.is_self(node.target) and m.matches(node.target.attr, m.Name()):
+                print(
+                    f"Found class variable {node.target.attr.value} without a type hint."
+                )
+
+        # Cover tuples.
 
     def visit_ClassDef(self, node: cst.ClassDef) -> Optional[bool]:
         if self.is_defined:  # We don't allow nested classes.
             raise AttributeError("Nested classes are not allowed.")
 
         self.is_defined = True
-        self.class_name = node.name.value
+        self.class_name = helpers.get_full_name_for_node(node)
 
     @staticmethod
     def create_stateful_fun(analyzed_tree: "ExtractStatefulFun") -> StatefulFun:
@@ -57,12 +62,17 @@ class ExtractStatefulEvent(cst.CSTVisitor):
 print(List[int])
 code = """
 class Test:
+    
     def fun(self):
+        self.z : Tuple[int, str]
         self.x: List[int] = 1
-        self.y = 0
+        self.y += 0
+        self.x: str = "3"
+        self.x, self.p = 3
+        self.r = 4
     
 """
-fun = ExtractStatefulFun()
 tree = cst.parse_module(code)
+fun = ExtractStatefulFun(module_node=tree)
 # print(tree)
 tree.visit(fun)
