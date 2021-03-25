c2b897b9f2e3fbe331898b673faca79b90b14257
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-03-25 16:50:05 +0100

    More unit tests for class wrappers

--- tests/wrapper/class_wrapper_test.py
+++ tests/wrapper/class_wrapper_test.py
@@ -4,6 +4,7 @@ from src.analysis.extract_class_descriptor import ExtractClassDescriptor
 from src.analysis.extract_method_descriptor import ExtractMethodDescriptor
 from src.descriptors import ClassDescriptor
 from src.dataflow.args import Arguments
+from src.dataflow.state import State
 
 import inspect
 import libcst as cst
@@ -63,3 +64,49 @@ class TestClassWrapper:
         args = Arguments({"name": "wouter"})
         res = wrapper.init_class(args)
         assert isinstance(res, FailedInvocation)
+
+    def test_unknown_method(self):
+        assert self.get_wrapper().find_method("UNKNOWN") is None
+
+    def test_str_repr(self):
+        assert str(self.get_wrapper()) == "ClassWrapper for the class SimpleClass."
+
+    def test_simple_invoke(self):
+        wrapper = self.get_wrapper()
+
+        state = State({"name": "wouter", "x": 5})
+        args = Arguments({"x": 5})
+        result = wrapper.invoke("update", state, args)
+
+        assert isinstance(result, InvocationResult)
+        assert result.return_results[0] == 0
+        assert result.updated_state.get() == {"name": "wouter", "x": 0}
+
+    def test_simple_invoke_argument_mismatch(self):
+        wrapper = self.get_wrapper()
+
+        state = State({"name": "wouter", "x": 5})
+        args = Arguments({"y": 5})
+        result = wrapper.invoke("update", state, args)
+
+        assert isinstance(result, FailedInvocation)
+
+    def test_simple_invoke_state_mismatch(self):
+        wrapper = self.get_wrapper()
+
+        state = State({"noname": "wouter"})
+        args = Arguments({"x": 5})
+        result = wrapper.invoke("update", state, args)
+
+        assert isinstance(result, FailedInvocation)
+
+    def test_simple_invoke_failed(self):
+        wrapper = self.get_wrapper()
+
+        setattr(wrapper.cls, "update", ...)
+
+        state = State({"name": "wouter", "x": 5})
+        args = Arguments({"x": 5})
+        result = wrapper.invoke("update", state, args)
+
+        assert isinstance(result, FailedInvocation)
