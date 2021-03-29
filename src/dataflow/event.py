28e6141e3062200785b91695725552066089f500
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-03-29 10:35:29 +0200

    Add the notion of a function address.

--- src/dataflow/event.py
+++ src/dataflow/event.py
@@ -1,19 +1,33 @@
 from src.dataflow.state import State
 from src.dataflow.args import Arguments
-from typing import List
+from typing import List, Optional
+
+
+class FunctionAddress:
+    """The address of a stateful or stateless function.
+
+    Consists of two parts:
+    - a FunctionType: the namespace and name of the function
+    - a key: an optional key, in case we deal with a stateful function.
+    For a stateless function, this key is None.
+
+    This address can be used to route an event correctly through a dataflow.
+    """
+
+    class FunctionType:
+        def __init__(self, namespace: str, name: str):
+            self.namespace = namespace
+            self.name = name
+
+    def __init__(self, function_type: FunctionType, key: Optional[str]):
+        self.function_type = function_type
+        self.key = key
+
+    def is_stateless(self):
+        return self.key is None
 
 
 class Event:
-    def __init__(
-        self,
-        class_name: str,
-        class_key: str,
-        func_name: str,
-        state: State,
-        args: Arguments,
-    ):
-        self.class_name = class_name
-        self.class_key = class_key
-        self.fun_name = func_name
-        self.state = state
+    def __init__(self, fun_address: FunctionAddress, args: Arguments):
+        self.fun_address = fun_address
         self.arguments = args
