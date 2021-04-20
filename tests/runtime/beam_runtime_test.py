c81111dbefc4a6190fc950b2d53fbf8df2409586
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-04-20 15:45:59 +0200

    Fixed correct output.

--- tests/runtime/beam_runtime_test.py
+++ tests/runtime/beam_runtime_test.py
@@ -2,14 +2,21 @@ from tests.common.common_classes import User, Item, stateflow
 from src.runtime.beam_runtime import BeamRuntime, Runtime
 from apache_beam.testing.test_stream import TestStream
 import apache_beam as beam
+from src.dataflow.event import Event, FunctionAddress, FunctionType, EventType
+from src.dataflow.args import Arguments
+import uuid
+from src.serialization.json_serde import JsonSerializer
 
 
 class TestBeamRuntime:
+    INPUT = TestStream()
+    OUTPUT = beam.Map(lambda x: x)
+
     def return_test_stream(self):
-        return TestStream()
+        return TestBeamRuntime.INPUT
 
     def identity(self, *args, **kwargs):
-        return beam.Map(lambda x: x)
+        return TestBeamRuntime.OUTPUT
 
     def setup_beam_runtime(self):
         runtime: BeamRuntime = BeamRuntime(stateflow.init())
@@ -19,5 +26,18 @@ class TestBeamRuntime:
         runtime.run()
 
     def test_runtime(self):
+        event_id: str = str(uuid.uuid4())
+        event: Event = Event(
+            event_id,
+            FunctionAddress(FunctionType("global", "User", True), None),
+            EventType.Request.InitClass,
+            {"args": Arguments({"username": "wouter"})},
+        )
+
+        event_serialized = JsonSerializer().serialize_event(event)
+
+        TestBeamRuntime.INPUT.add_elements(
+            [(bytes(event_id, "utf-8"), event_serialized)]
+        )
         self.setup_beam_runtime()
-        pass
+        print("HERE")
