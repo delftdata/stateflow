67f2af01372aa60fb2be609236da491f58c144ac
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-04-20 10:54:53 +0200

    SerDe to interface

--- src/serialization/serde.py
+++ src/serialization/serde.py
@@ -1,16 +1,21 @@
-from src.dataflow.event import Event, FunctionAddress
+from src.dataflow.event import Event
+import abc
 from typing import Dict
 
 
-class SerDe:
+class SerDe(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
     def serialize_event(self, event: Event) -> bytes:
         pass
 
+    @abc.abstractmethod
     def deserialize_event(self, event: bytes) -> Event:
         pass
 
+    @abc.abstractmethod
     def serialize_dict(self, dict: Dict) -> bytes:
         pass
 
+    @abc.abstractmethod
     def deserialize_dict(self, dict: bytes) -> Dict:
         pass
