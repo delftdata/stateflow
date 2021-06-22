b4a1a5407c651a06d5032b7c133572c183e1883a
Wouter Zorgdrager <zorgdragerw@gmail.com>
2021-06-22 12:33:10 +0200

    More fast api

--- src/client/fastapi/FastAPIClient.py
+++ src/client/fastapi/FastAPIClient.py
@@ -2,11 +2,15 @@ from fastapi import FastAPI
 from src.dataflow.dataflow import Dataflow, ClassDescriptor
 from src.descriptors.method_descriptor import MethodDescriptor
 from src.dataflow.dataflow import Operator
+from aiokafka import AIOKafkaProducer
 
 
 class FastAPIClient:
     def __init__(self, flow: Dataflow):
         self.app: FastAPI = FastAPI()
+        self.producer: AIOKafkaProducer = None
+
+        self.setup_init()
 
         for operator in flow.operators:
             cls_descriptor: ClassDescriptor = operator.class_wrapper.class_desc
@@ -14,23 +18,41 @@ class FastAPIClient:
 
             for method in cls_descriptor.methods_dec:
                 if not self.get_name(method) == "__key__":
-                    self.get_handler(fun_type, method)
+                    self.create_method_handler(fun_type, method)
 
-            self.get_handler(
-                fun_type,
-            )
+            self.create_fun_get(fun_type)
+
+    def setup_init(self):
+        @self.app.on_event("startup")
+        async def setup_kafka():
+            self.producer = AIOKafkaProducer(bootstrap_servers="localhost:9092")
+            await self.producer.start()
+
+        @self.app.on_event("shutdown")
+        async def stop_kafka():
+            await self.producer.close()
+
+        return setup_kafka
 
     def get_name(self, method: MethodDescriptor) -> str:
         if method.method_name == "__init__":
             return "create"
         return method.method_name
 
-    def get_handler(
-        self, function_type, method_desc: MethodDescriptor, method: str = "POST"
-    ):
-        @self.app.api_route(
+    def create_fun_get(self, function_type):
+        @self.app.get(
+            f"/stateflow/{function_type.get_full_name()}/find/",
+            name=f"find_{function_type.get_full_name()}",
+        )
+        async def endpoint():
+            return {"Hey I am": f"create: {function_type.to_dict()}"}
+
+        return endpoint
+
+    def create_method_handler(self, function_type, method_desc: MethodDescriptor):
+        @self.app.post(
             f"/stateflow/{function_type.get_full_name()}/{self.get_name(method_desc)}",
-            methods=[method],
+            name=self.get_name(method_desc),
         )
         async def endpoint():
             return {"Hey I am": method_desc.method_name}
